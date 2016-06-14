f9e7709eaf110dcc50331937305511bf4ef584f4
roimartins <roi.martinez@cefas.co.uk>
2016-06-14 09:46:03 +0100

    commit r script file

---
+++ R/postgisgeom.R
@@ -0,0 +1,83 @@
+#' PostGIs Polygons
+#'
+#' This function convert postgis polygons into r spatial objects 
+#'
+#' @param us User Databse Name
+#' @param pw Password
+#' @export
+#' @examples
+#' postgisgeom(us="Tomas",pw = "Greif12")
+#' postgisgeom(us="Your", pw = "Name12")
+
+
+
+library(devtools)
+
+getwd()
+create("rpackage")
+
+library(RODBC)
+library(RPostgreSQL)
+library(rgeos)          # Geostatistical Package developed for R platform /  geometry operations
+library(rgdal)
+
+### get polygons from mpa areas ###
+
+
+pw <- {
+  "postgres1233"
+}
+
+
+tt <- postgisgeom("postgres","postgres1234")
+
+postgisgeom <- function(us, pw ) {
+  
+  
+  
+  # loads the PostgreSQL driver
+  drv <- dbDriver("PostgreSQL")
+  
+  # creates a connection to the postgres database
+  # note that "con" will be used later in each connection to the database
+  con <- dbConnect(drv, dbname = "postgis",
+                   host = "localhost", port = 5433,
+                   user = us, password = pw)
+  
+  rm(pw) #removes the password
+  
+  
+  
+  rr <- dbGetQuery(con, "select gid,site_name, st_asewkt(geom) as geom from geography_general.ospar_mpas")
+  
+  dbDisconnect(con)
+  dbUnloadDriver(drv)
+  
+  
+  geom <- rr$geom
+  geomdesc <- rr$gid
+  geomname<- rr$site_name
+  
+  spgeom <- strsplit(rr[, "geom"],";")
+  spgeomprj <- lapply(spgeom, function (x) substr(x[1],6, nchar(x[1])))
+  spgeomg <- lapply(spgeom, function (x) x[2])
+  
+  spmpadf <- rr[,c("gid","site_name")]
+  row.names(spmpadf) <- spmpadf$gid
+  
+  geoms <- lapply(spgeomg, function (x) readWKT(x,p4s=CRS(paste0("+init=epsg:",spgeomprj))))
+  
+  listpol<-list()
+  
+  for (i in 1:length(geoms)) {
+    geoms[[i]]@polygons[[1]]@ID <- as.character(geomdesc[i]  )
+    listpol[[i]]<-geoms[[i]]@polygons[[1]]
+  }
+  
+  spmmpas <- SpatialPolygons(listpol)
+  proj4string(spmmpas) <- CRS("+init=epsg:4326")
+  spmmpasdf <- SpatialPolygonsDataFrame(spmmpas,spmpadf, match.ID = T)
+  
+  return(spmmpasdf)
+  
+}
